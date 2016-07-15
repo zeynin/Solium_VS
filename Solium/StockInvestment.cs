83db0206755c4dacde2355c37855c0de59f54ad5
Zeynin <zeynin@hotmail.com>
2016-07-14 20:50:13 -0600

    Added unit test support and first investments class

---
+++ Solium/StockInvestment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solium
+{
+    class StockInvestment
+    {
+        DateTime grantDate;
+        int shares;
+        float grantPrice;
+
+        DateTime perfDate;
+        float performanceMultiplier;
+
+        DateTime saleDate;
+        int soldShares;
+        float salePrice;
+
+        ~StockInvestment()
+        {
+            shares = 0;
+            grantPrice = 0;
+            performanceMultiplier = 1;
+            soldShares = 0;
+            salePrice = 0;
+
+            //DateTime.ParseExact( string_datetime, "YYYYMMdd", CultureInfo.InvariantCulture );
+            //string[] tokens = published.Split( ',' );
+        }
+
+    }
+}
